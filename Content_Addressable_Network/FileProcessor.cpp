f6f383e0f64403bb57998015f4ab19bbfb5217aa
Balakrishna Lellapalli <bklellapalli@gmail.com>
2017-05-11 21:53:49 -0400

    05-11-2017 09:53 pm Fileprocessor

---
+++ Content_Addressable_Network/FileProcessor.cpp
@@ -0,0 +1,62 @@
+/***************Server.cpp******************
+ * FILE NAME: Fileprocessor.cpp
+ *
+ * Created by Balakrishna on 5/11/17
+ * DESCRIPTION: Definition of all Fileprocessor related class
+ *
+ * Copyright © 2017 Balakrishna. All rights reserved.
+ **********************************/
+
+#include "FileProcessor.hpp"
+
+FileProcessor::FileProcessor(std::string ipAddress, std::string port)
+{
+    std::string url;
+    mongoc_init ();
+    iov.iov_base = (void *) buf;
+    iov.iov_len = sizeof buf;
+    
+    /* connect to localhost client */
+    url = "mongodb://" + ipAddress + ":" + port + "?appname=CAN-Protocol";
+    
+    client = mongoc_client_new (url.c_str());
+    mongoc_client_set_error_api (client, 2);
+    gridfs = mongoc_client_get_gridfs (client, "CAN", "fs", &error);
+}
+
+FileProcessor::~FileProcessor()
+{
+    mongoc_gridfs_destroy (gridfs);
+    mongoc_client_destroy (client);
+    mongoc_cleanup ();
+}
+
+void FileProcessor::readFile(std::string fileName)
+{
+    file = mongoc_gridfs_find_one_by_filename (gridfs, fileName.c_str(), &error);
+    stream = mongoc_stream_gridfs_new (file);
+    for (;;)
+    {
+        r = mongoc_stream_readv (stream, &iov, 1, -1, 0);
+        if (r == 0) { break; }
+        
+        FILE *fp;
+        fp = fopen( fileName.c_str() , "w" );
+        fwrite (iov.iov_base, 1, r, fp);
+        fclose(fp);
+    }
+    mongoc_stream_destroy (stream);
+    mongoc_gridfs_file_destroy (file);
+}
+
+void FileProcessor::writeFile(std::string fileName)
+{
+    stream = mongoc_stream_file_new_for_path (fileName.c_str(), O_RDONLY, 0);
+    opt.filename = fileName.c_str();
+    
+    /* the driver generates a file_id for you */
+    file = mongoc_gridfs_create_file_from_stream (gridfs, stream, &opt);
+    
+    mongoc_gridfs_file_save (file);
+    mongoc_gridfs_file_destroy (file);
+}
---
+++ Content_Addressable_Network/FileProcessor.hpp
@@ -0,0 +1,33 @@
+#ifndef Fileprocessor_hpp
+#define Fileprocessor_hpp
+
+#include <string>
+#include <mongoc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 4096
+
+class FileProcessor
+{
+    private:
+        mongoc_gridfs_t *gridfs;
+        mongoc_gridfs_file_t *file;
+        mongoc_gridfs_file_opt_t opt = {0};
+        mongoc_client_t *client;
+        mongoc_stream_t *stream;
+        bson_error_t error;
+        mongoc_iovec_t iov;
+    
+        char buf[BUFFER_SIZE];
+        ssize_t r;
+    
+public:
+        FileProcessor(std::string ipAddress, std::string port);
+        ~FileProcessor();
+        void readFile(std::string fileName);
+        void writeFile(std::string fileName);
+};
+
+#endif /* Fileprocessor_hpp */
